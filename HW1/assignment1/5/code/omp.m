2affd3c5837e5f57174a009a76f8820e6f86b483
Saksham Rathi <22b1003@iitb.ac.in>
2025-02-01 19:36:35 +0530

    q5 omp results corrected

--- HW1/assignment1/5/code/omp.m
+++ HW1/assignment1/5/code/omp.m
@@ -1,88 +1,129 @@
-function sparse_image_reconstruction_omp()
-    N = 32;
-    image_size = N * N;
-    k_values = [5, 50, 200];
-    m_values = 100:100:1000;
+tic;
 
-    DCT_basis = dctmtx(N);
-    DCT2_basis = kron(DCT_basis, DCT_basis);
+image_size = 32;
+algo_name = "OMP";
+set(0, 'DefaultFigureVisible', 'off');
 
-    for k = k_values
-        f = generate_sparse_image(DCT2_basis, k, N);
-        imwrite(reshape(f, N, N), sprintf('../images/omp/Ground_Truth_k_%d.png', k));
+k_values = [5, 10, 20, 30, 50, 100, 150, 200];
+m_values = 100:100:1000;
 
-        rmse_vs_m = zeros(size(m_values));
+plot_k_idxs = [1,5,8];
+plot_m_idxs = [5,7];
 
-        for idx = 1:length(m_values)
-            m = m_values(idx);
-            Phi = radamacher_matrix(m, image_size);
-            y = Phi * f(:);
-            f_hat = omp_algo(Phi, y, k);
-            rmse = norm(f(:) - f_hat) / norm(f(:));
-            rmse_vs_m(idx) = rmse;
+RMSE = zeros(length(k_values), length(m_values));
+ORIG_IMAGES = zeros(image_size, image_size, length(k_values));
+RECON_IMAGES_P1 = zeros(image_size, image_size, length(m_values), length(plot_k_idxs));
+RECON_IMAGES_P2 = zeros(image_size, image_size, length(k_values), length(plot_m_idxs));
 
-            imwrite(reshape(f_hat, N, N), ...
-                sprintf('../images/omp/Reconstructed_k_%d_m_%d.png', k, m));
+for idx_k = 1:length(k_values)
+    k = k_values(idx_k);
+    coefficients = zeros(image_size^2, 1);
+    selected_indices = randperm(image_size^2, k);
+    coefficients(selected_indices) = randn(k, 1);
+    psi = dctmtx(image_size^2);
+    vec_f = psi * coefficients;
+    f = reshape(vec_f, image_size, image_size);
+
+    ORIG_IMAGES(:, :, idx_k) = f;
+    
+    for idx_m = 1:length(m_values)
+        m = m_values(idx_m);
+        phi = randi([0, 1], m, image_size^2);
+        phi(phi == 0) = -1;
+        A = phi * psi;
+        y = phi * vec_f;
+
+        theta = OMP(A, y, k, image_size);
+        f_hat = reshape(psi*theta, image_size, image_size);
+        if any(plot_k_idxs == idx_k)
+          RECON_IMAGES_P1(:, :, idx_m, find(plot_k_idxs == idx_k)) = f_hat;
         end
-
-        figure;
-        plot(m_values, rmse_vs_m, '-o');
-        xlabel('Number of measurements (m)');
-        ylabel('RMSE');
-        title(sprintf('RMSE vs m (k = %d)', k));
-        saveas(gcf, sprintf('../images/omp/RMSE_vs_m_k_%d.png', k));
-        close;
-    end
-
-    m_fixed = [500, 700];
-    k_values_all = [5, 10, 20, 30, 50, 100, 150, 200];
-    for m = m_fixed
-        rmse_vs_k = zeros(size(k_values_all));
-
-        for idx = 1:length(k_values_all)
-            k = k_values_all(idx);
-            f = generate_sparse_image(DCT2_basis, k, N);
-            Phi = radamacher_matrix(m, image_size);
-            y = Phi * f(:);
-            f_hat = omp_algo(Phi, y, k);
-            rmse = norm(f(:) - f_hat) / norm(f(:));
-            rmse_vs_k(idx) = rmse;
+        if any(plot_m_idxs == idx_m)
+          RECON_IMAGES_P2(:, :, idx_k, find(plot_m_idxs == idx_m)) = f_hat;
         end
-
-        figure;
-        plot(k_values_all, rmse_vs_k, '-o');
-        xlabel('Sparsity Level (k)');
-        ylabel('RMSE');
-        title(sprintf('RMSE vs k (m = %d)', m));
-        saveas(gcf, sprintf('../images/omp/RMSE_vs_k_m_%d.png', m));
-        close;
+        RMSE(idx_k, idx_m) = norm(vec_f - f_hat(:)) / norm(vec_f);
     end
 end
 
-function f = generate_sparse_image(DCT2_basis, k, N)
-    coeffs = zeros(N^2, 1);
-    selected_indices = randperm(N^2, k);
-    coeffs(selected_indices) = randn(k, 1);
-    f = DCT2_basis * coeffs;
+% PLOTS
+
+for i = 1:length(k_values)
+  z = ORIG_IMAGES(:,:,i);
+  imwrite(z, sprintf("../images/omp/Ground_Truth_k_%d.png", k_values(i)));
 end
 
-function Phi = radamacher_matrix(m, n)
-    Phi = sign(randn(m, n));
+for i = 1:length(plot_k_idxs)
+  for j = 1:length(m_values)
+    z = RECON_IMAGES_P1(:,:,j,i);
+    imwrite(z, sprintf("../images/omp/Reconstructed_k_%d_m_%d.png", k_values(plot_k_idxs(i)), m_values(j)));
+  end
 end
 
-function x = omp_algo(Phi, y, k)
-    residual = y;
-    idx_set = [];
-    x = zeros(size(Phi, 2), 1);
+for i = 1:length(plot_m_idxs)
+  for j = 1:length(k_values)
+    z = RECON_IMAGES_P2(:,:,j,i);
+    imwrite(z, sprintf("../images/omp/Reconstructed_k_%d_m_%d.png", k_values(j), m_values(plot_m_idxs(i))));
+  end
+end
 
-    for i = 1:k
-        correlations = abs(Phi' * residual);
-        [~, idx] = max(correlations);
-        idx_set = [idx_set, idx];
-        Phi_restricted = Phi(:, idx_set);
-        x_restricted = Phi_restricted \ y;
-        residual = y - Phi_restricted * x_restricted;
+figure;
+hold on;
+legends = [];
+for idx = 1:length(plot_k_idxs)
+    k_idx = plot_k_idxs(idx);
+    k = k_values(k_idx);
+
+    y_axis = RMSE(k_idx,:);
+    x_axis = m_values;
+    plot(x_axis, y_axis);
+    legends = [legends, sprintf("k=%d",k)];
+end
+xlabel('m');
+ylabel('RMSE');
+title(sprintf("RMSE vs m for varying k - %s", algo_name));
+legend(legends);
+hold off;
+saveas(gcf, sprintf("../images/omp/omp_k.png"));
+
+figure;
+hold on;
+legends = [];
+for idx = 1:length(plot_m_idxs)
+    m_idx = plot_m_idxs(idx);
+    m = m_values(m_idx);
+
+    y_axis = RMSE(:,m_idx);
+    x_axis = k_values;
+    plot(x_axis, y_axis);
+    legends = [legends, sprintf("m=%d",m)];
+end
+xlabel('k');
+ylabel('RMSE');
+title(sprintf("RMSE vs k for varying m - %s", algo_name));
+legend(legends);
+hold off;
+saveas(gcf, sprintf("../images/omp/omp_m.png"));
+
+toc;
+
+function res = OMP(A, y, image_size, k)
+    [m, n] = size(A);
+    norm_A = A ./ sqrt(sum(A.^2, 1));
+    r = y;
+    T = [];
+    iterations = 0;
+
+    while iterations < m && norm(r)^2 > 1e-10
+        [~, j] = max(abs(norm_A' * r));
+        if ismember(j,T)
+          break;
+        end
+        T = [T, j];
+        theta = A(:, T) \ y;
+        r = y - A(:, T) * theta;
+        iterations = iterations + 1;
     end
 
-    x(idx_set) = x_restricted;
+    res = zeros(n, 1);
+    res(T) = theta;
 end
