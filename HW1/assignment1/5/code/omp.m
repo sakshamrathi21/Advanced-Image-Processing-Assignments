350715fad97f3d0ea45457bba000ea15d5ec6df1
Saksham Rathi <22b1003@iitb.ac.in>
2025-02-01 21:57:12 +0530

    q5 cosamp code complete

--- HW1/assignment1/5/code/omp.m
+++ HW1/assignment1/5/code/omp.m
@@ -2,7 +2,6 @@ tic;
 
 image_size = 32;
 algo_name = "OMP";
-set(0, 'DefaultFigureVisible', 'off');
 
 k_values = [5, 10, 20, 30, 50, 100, 150, 200];
 m_values = 100:100:1000;
@@ -11,57 +10,50 @@ plot_k_idxs = [1,5,8];
 plot_m_idxs = [5,7];
 
 RMSE = zeros(length(k_values), length(m_values));
-ORIG_IMAGES = zeros(image_size, image_size, length(k_values));
-RECON_IMAGES_P1 = zeros(image_size, image_size, length(m_values), length(plot_k_idxs));
-RECON_IMAGES_P2 = zeros(image_size, image_size, length(k_values), length(plot_m_idxs));
+original_image = zeros(image_size, image_size, length(k_values));
+reconstructed_images_1 = zeros(image_size, image_size, length(m_values), length(plot_k_idxs));
+reconstructed_images_2 = zeros(image_size, image_size, length(k_values), length(plot_m_idxs));
 
-for idx_k = 1:length(k_values)
-    k = k_values(idx_k);
+for k = k_values
     coefficients = zeros(image_size^2, 1);
     selected_indices = randperm(image_size^2, k);
     coefficients(selected_indices) = randn(k, 1);
     psi = dctmtx(image_size^2);
     vec_f = psi * coefficients;
     f = reshape(vec_f, image_size, image_size);
-
-    ORIG_IMAGES(:, :, idx_k) = f;
-    
-    for idx_m = 1:length(m_values)
-        m = m_values(idx_m);
+    original_image(:, :, idx_k) = f;
+    for m = m_values
         phi = randi([0, 1], m, image_size^2);
         phi(phi == 0) = -1;
         A = phi * psi;
         y = phi * vec_f;
-
-        theta = OMP(A, y, k, image_size);
+        theta = OMP(A, y, k);
         f_hat = reshape(psi*theta, image_size, image_size);
         if any(plot_k_idxs == idx_k)
-          RECON_IMAGES_P1(:, :, idx_m, find(plot_k_idxs == idx_k)) = f_hat;
+          reconstructed_images_1(:, :, idx_m, find(plot_k_idxs == idx_k)) = f_hat;
         end
         if any(plot_m_idxs == idx_m)
-          RECON_IMAGES_P2(:, :, idx_k, find(plot_m_idxs == idx_m)) = f_hat;
+          reconstructed_images_2(:, :, idx_k, find(plot_m_idxs == idx_m)) = f_hat;
         end
         RMSE(idx_k, idx_m) = norm(vec_f - f_hat(:)) / norm(vec_f);
     end
 end
 
-% PLOTS
-
 for i = 1:length(k_values)
-  z = ORIG_IMAGES(:,:,i);
+  z = original_image(:,:,i);
   imwrite(z, sprintf("../images/omp/Ground_Truth_k_%d.png", k_values(i)));
 end
 
 for i = 1:length(plot_k_idxs)
   for j = 1:length(m_values)
-    z = RECON_IMAGES_P1(:,:,j,i);
+    z = reconstructed_images_1(:,:,j,i);
     imwrite(z, sprintf("../images/omp/Reconstructed_k_%d_m_%d.png", k_values(plot_k_idxs(i)), m_values(j)));
   end
 end
 
 for i = 1:length(plot_m_idxs)
   for j = 1:length(k_values)
-    z = RECON_IMAGES_P2(:,:,j,i);
+    z = reconstructed_images_2(:,:,j,i);
     imwrite(z, sprintf("../images/omp/Reconstructed_k_%d_m_%d.png", k_values(j), m_values(plot_m_idxs(i))));
   end
 end
@@ -106,7 +98,7 @@ saveas(gcf, sprintf("../images/omp/omp_m.png"));
 
 toc;
 
-function res = OMP(A, y, image_size, k)
+function res = OMP(A, y, k)
     [m, n] = size(A);
     norm_A = A ./ sqrt(sum(A.^2, 1));
     r = y;
