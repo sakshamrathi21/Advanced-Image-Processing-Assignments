47b3a554af432282d09cebf21b2e691e83198792
Saksham Rathi <22b1003@iitb.ac.in>
2025-02-05 17:29:49 +0530

    submission

--- HW1/assignment1/5/code/cosamp.m
+++ HW1/assignment1/5/code/cosamp.m
@@ -1,18 +1,16 @@
 tic;
 
 image_size = 32;
-algo_name = "COSAMP";
-
+algo_name = "COSAMP"; % Name of the algorithm
 k_values = [5, 10, 20, 30, 50, 100, 150, 200];
 m_values = 100:100:1000;
-
-plot_k_idxs = [1,5,8];
-plot_m_idxs = [5,7];
+k_indices_to_plot = [1,5,8];
+m_indices_to_plot = [5,7];
 
 RMSE = zeros(length(k_values), length(m_values));
 original_image = zeros(image_size, image_size, length(k_values));
-reconstructed_images_1 = zeros(image_size, image_size, length(m_values), length(plot_k_idxs));
-reconstructed_images_2 = zeros(image_size, image_size, length(k_values), length(plot_m_idxs));
+reconstructed_images_1 = zeros(image_size, image_size, length(m_values), length(k_indices_to_plot));
+reconstructed_images_2 = zeros(image_size, image_size, length(k_values), length(m_indices_to_plot));
 
 for idx_k = 1:length(k_values)
     k = k_values(idx_k);
@@ -29,42 +27,43 @@ for idx_k = 1:length(k_values)
         phi(phi == 0) = -1;
         A = phi * psi;
         y = phi * vec_f;
-        theta = algo(A, y, k);
+        theta = cosamp_algo(A, y, k);
         f_hat = reshape(psi*theta, image_size, image_size);
-        if any(plot_k_idxs == idx_k)
-          reconstructed_images_1(:, :, idx_m, find(plot_k_idxs == idx_k)) = f_hat;
+        if any(k_indices_to_plot == idx_k)
+          reconstructed_images_1(:, :, idx_m, find(k_indices_to_plot == idx_k)) = f_hat;
         end
-        if any(plot_m_idxs == idx_m)
-          reconstructed_images_2(:, :, idx_k, find(plot_m_idxs == idx_m)) = f_hat;
+        if any(m_indices_to_plot == idx_m)
+          reconstructed_images_2(:, :, idx_k, find(m_indices_to_plot == idx_m)) = f_hat;
         end
         RMSE(idx_k, idx_m) = norm(vec_f - f_hat(:)) / norm(vec_f);
     end
 end
 
+
+% Save the ground truth and reconstructed images
 for i = 1:length(k_values)
   z = original_image(:,:,i);
   imwrite(z, sprintf("../images/cosamp/Ground_Truth_k_%d.png", k_values(i)));
 end
-
-for i = 1:length(plot_k_idxs)
+for i = 1:length(k_indices_to_plot)
   for j = 1:length(m_values)
     z = reconstructed_images_1(:,:,j,i);
-    imwrite(z, sprintf("../images/cosamp/Reconstructed_k_%d_m_%d.png", k_values(plot_k_idxs(i)), m_values(j)));
+    imwrite(z, sprintf("../images/cosamp/Reconstructed_k_%d_m_%d.png", k_values(k_indices_to_plot(i)), m_values(j)));
   end
 end
-
-for i = 1:length(plot_m_idxs)
+for i = 1:length(m_indices_to_plot)
   for j = 1:length(k_values)
     z = reconstructed_images_2(:,:,j,i);
-    imwrite(z, sprintf("../images/cosamp/Reconstructed_k_%d_m_%d.png", k_values(j), m_values(plot_m_idxs(i))));
+    imwrite(z, sprintf("../images/cosamp/Reconstructed_k_%d_m_%d.png", k_values(j), m_values(m_indices_to_plot(i))));
   end
 end
 
+% Plot RMSE vs k and m
 figure;
 hold on;
 legends = [];
-for idx = 1:length(plot_k_idxs)
-    k_idx = plot_k_idxs(idx);
+for idx = 1:length(k_indices_to_plot)
+    k_idx = k_indices_to_plot(idx);
     k = k_values(k_idx);
 
     y_axis = RMSE(k_idx,:);
@@ -78,12 +77,11 @@ title(sprintf("RMSE vs m for varying k - %s", algo_name));
 legend(legends);
 hold off;
 saveas(gcf, sprintf("../images/cosamp/cosamp_k.png"));
-
 figure;
 hold on;
 legends = [];
-for idx = 1:length(plot_m_idxs)
-    m_idx = plot_m_idxs(idx);
+for idx = 1:length(m_indices_to_plot)
+    m_idx = m_indices_to_plot(idx);
     m = m_values(m_idx);
 
     y_axis = RMSE(:,m_idx);
@@ -100,29 +98,8 @@ saveas(gcf, sprintf("../images/cosamp/cosamp_m.png"));
 
 toc;
 
-function res = OMP(A, y, k)
-    [m, n] = size(A);
-    norm_A = A ./ sqrt(sum(A.^2, 1));
-    r = y;
-    T = [];
-    iterations = 0;
-
-    while iterations < m && norm(r)^2 > 1e-10
-        [~, j] = max(abs(norm_A' * r));
-        if ismember(j,T)
-          break;
-        end
-        T = [T, j];
-        theta = A(:, T) \ y;
-        r = y - A(:, T) * theta;
-        iterations = iterations + 1;
-    end
-
-    res = zeros(n, 1);
-    res(T) = theta;
-end
-
-function [Sest, iterations, error] = algo(Phi,u,K)
+function [Sest, iterations, error] = cosamp_algo(Phi,u,K)
+  % CoSaMP: Compressive Sampling Matching Pursuit
     Sest = zeros(size(Phi,2),1);
     tol = 1e-6;
     maxiterations = K;
--- HW1/assignment1/5/code/omp.m
+++ HW1/assignment1/5/code/omp.m
@@ -1,123 +1,114 @@
 tic;
+img_dim = 32;
+algorithm_name = "OMP"; % Updated algorithm name
+sparsity_levels = [5, 10, 20, 30, 50, 100, 150, 200];
+sample_sizes = 100:100:1000;
+sparsity_plot_indices = [1,5,8];
+sample_plot_indices = [5,7];
+error_matrix = zeros(length(sparsity_levels), length(sample_sizes));
+true_image = zeros(img_dim, img_dim, length(sparsity_levels));
+recon_images_1 = zeros(img_dim, img_dim, length(sample_sizes), length(sparsity_plot_indices));
+recon_images_2 = zeros(img_dim, img_dim, length(sparsity_levels), length(sample_plot_indices));
 
-image_size = 32;
-algo_name = "OMP";
-
-k_values = [5, 10, 20, 30, 50, 100, 150, 200];
-m_values = 100:100:1000;
-
-plot_k_idxs = [1,5,8];
-plot_m_idxs = [5,7];
-
-RMSE = zeros(length(k_values), length(m_values));
-original_image = zeros(image_size, image_size, length(k_values));
-reconstructed_images_1 = zeros(image_size, image_size, length(m_values), length(plot_k_idxs));
-reconstructed_images_2 = zeros(image_size, image_size, length(k_values), length(plot_m_idxs));
-
-for idx_k = 1:length(k_values)
-    k = k_values(idx_k);
-    coefficients = zeros(image_size^2, 1);
-    selected_indices = randperm(image_size^2, k);
-    coefficients(selected_indices) = randn(k, 1);
-    psi = dctmtx(image_size^2);
-    vec_f = psi * coefficients;
-    f = reshape(vec_f, image_size, image_size);
-    original_image(:, :, idx_k) = f;
-    for idx_m = 1:length(m_values)
-        m = m_values(idx_m);
-        phi = randi([0, 1], m, image_size^2);
-        phi(phi == 0) = -1;
-        A = phi * psi;
-        y = phi * vec_f;
-        theta = OMP(A, y, k);
-        f_hat = reshape(psi*theta, image_size, image_size);
-        if any(plot_k_idxs == idx_k)
-          reconstructed_images_1(:, :, idx_m, find(plot_k_idxs == idx_k)) = f_hat;
+for s_idx = 1:length(sparsity_levels)
+    s = sparsity_levels(s_idx);
+    coeff_vector = zeros(img_dim^2, 1);
+    chosen_indices = randperm(img_dim^2, s);
+    coeff_vector(chosen_indices) = randn(s, 1);
+    transform_matrix = dctmtx(img_dim^2);
+    signal_vector = transform_matrix * coeff_vector;
+    reshaped_image = reshape(signal_vector, img_dim, img_dim);
+    true_image(:, :, s_idx) = reshaped_image;
+    
+    for sm_idx = 1:length(sample_sizes)
+        sm = sample_sizes(sm_idx);
+        sensing_matrix = randi([0, 1], sm, img_dim^2);
+        sensing_matrix(sensing_matrix == 0) = -1;
+        measurement_matrix = sensing_matrix * transform_matrix;
+        observations = sensing_matrix * signal_vector;
+        estimated_coeffs = OMP_Algo(measurement_matrix, observations, s);
+        reconstructed_image = reshape(transform_matrix * estimated_coeffs, img_dim, img_dim);
+        
+        if any(sparsity_plot_indices == s_idx)
+          recon_images_1(:, :, sm_idx, find(sparsity_plot_indices == s_idx)) = reconstructed_image;
         end
-        if any(plot_m_idxs == idx_m)
-          reconstructed_images_2(:, :, idx_k, find(plot_m_idxs == idx_m)) = f_hat;
+        if any(sample_plot_indices == sm_idx)
+          recon_images_2(:, :, s_idx, find(sample_plot_indices == sm_idx)) = reconstructed_image;
         end
-        RMSE(idx_k, idx_m) = norm(vec_f - f_hat(:)) / norm(vec_f);
+        
+        error_matrix(s_idx, sm_idx) = norm(signal_vector - reconstructed_image(:)) / norm(signal_vector);
     end
 end
 
-for i = 1:length(k_values)
-  z = original_image(:,:,i);
-  imwrite(z, sprintf("../images/omp/Ground_Truth_k_%d.png", k_values(i)));
+% Save images
+for i = 1:length(sparsity_levels)
+  imwrite(true_image(:,:,i), sprintf("../images/omp/Ground_Truth_k_%d.png", sparsity_levels(i)));
 end
-
-for i = 1:length(plot_k_idxs)
-  for j = 1:length(m_values)
-    z = reconstructed_images_1(:,:,j,i);
-    imwrite(z, sprintf("../images/omp/Reconstructed_k_%d_m_%d.png", k_values(plot_k_idxs(i)), m_values(j)));
+for i = 1:length(sparsity_plot_indices)
+  for j = 1:length(sample_sizes)
+    imwrite(recon_images_1(:,:,j,i), sprintf("../images/omp/Reconstructed_k_%d_m_%d.png", sparsity_levels(sparsity_plot_indices(i)), sample_sizes(j)));
+  end
+end
+for i = 1:length(sample_plot_indices)
+  for j = 1:length(sparsity_levels)
+    imwrite(recon_images_2(:,:,j,i), sprintf("../images/omp/Reconstructed_k_%d_m_%d.png", sparsity_levels(j), sample_sizes(sample_plot_indices(i))));
   end
 end
 
-for i = 1:length(plot_m_idxs)
-  for j = 1:length(k_values)
-    z = reconstructed_images_2(:,:,j,i);
-    imwrite(z, sprintf("../images/omp/Reconstructed_k_%d_m_%d.png", k_values(j), m_values(plot_m_idxs(i))));
-  end
-end
-
+% Plot RMSE
 figure;
 hold on;
-legends = [];
-for idx = 1:length(plot_k_idxs)
-    k_idx = plot_k_idxs(idx);
-    k = k_values(k_idx);
-
-    y_axis = RMSE(k_idx,:);
-    x_axis = m_values;
-    plot(x_axis, y_axis);
-    legends = [legends, sprintf("k=%d",k)];
+legend_entries = [];
+for idx = 1:length(sparsity_plot_indices)
+    s_idx = sparsity_plot_indices(idx);
+    s = sparsity_levels(s_idx);
+    plot(sample_sizes, error_matrix(s_idx,:));
+    legend_entries = [legend_entries, sprintf("k=%d", s)];
 end
 xlabel('m');
 ylabel('RMSE');
-title(sprintf("RMSE vs m for varying k - %s", algo_name));
-legend(legends);
+title(sprintf("RMSE vs m for varying k - %s", algorithm_name));
+legend(legend_entries);
 hold off;
 saveas(gcf, sprintf("../images/omp/omp_k.png"));
 
 figure;
 hold on;
-legends = [];
-for idx = 1:length(plot_m_idxs)
-    m_idx = plot_m_idxs(idx);
-    m = m_values(m_idx);
-
-    y_axis = RMSE(:,m_idx);
-    x_axis = k_values;
-    plot(x_axis, y_axis);
-    legends = [legends, sprintf("m=%d",m)];
+legend_entries = [];
+for idx = 1:length(sample_plot_indices)
+    sm_idx = sample_plot_indices(idx);
+    sm = sample_sizes(sm_idx);
+    plot(sparsity_levels, error_matrix(:,sm_idx));
+    legend_entries = [legend_entries, sprintf("m=%d", sm)];
 end
 xlabel('k');
 ylabel('RMSE');
-title(sprintf("RMSE vs k for varying m - %s", algo_name));
-legend(legends);
+title(sprintf("RMSE vs k for varying m - %s", algorithm_name));
+legend(legend_entries);
 hold off;
 saveas(gcf, sprintf("../images/omp/omp_m.png"));
 
 toc;
 
-function res = OMP(A, y, k)
-    [m, n] = size(A);
-    norm_A = A ./ sqrt(sum(A.^2, 1));
-    r = y;
-    T = [];
-    iterations = 0;
+function result = OMP_Algo(A, y, sparsity)
+    % Modified Orthogonal Matching Pursuit
+    [num_rows, num_cols] = size(A);
+    normalized_A = A ./ sqrt(sum(A.^2, 1));
+    residual = y;
+    selected_atoms = [];
+    iter = 0;
 
-    while iterations < m && norm(r)^2 > 1e-10
-        [~, j] = max(abs(norm_A' * r));
-        if ismember(j,T)
+    while iter < num_rows && norm(residual)^2 > 1e-10
+        [~, max_idx] = max(abs(normalized_A' * residual));
+        if ismember(max_idx, selected_atoms)
           break;
         end
-        T = [T, j];
-        theta = A(:, T) \ y;
-        r = y - A(:, T) * theta;
-        iterations = iterations + 1;
+        selected_atoms = [selected_atoms, max_idx];
+        theta_estimate = A(:, selected_atoms) \ y;
+        residual = y - A(:, selected_atoms) * theta_estimate;
+        iter = iter + 1;
     end
 
-    res = zeros(n, 1);
-    res(T) = theta;
+    result = zeros(num_cols, 1);
+    result(selected_atoms) = theta_estimate;
 end
